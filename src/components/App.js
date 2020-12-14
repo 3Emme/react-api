00ef853d02070754d87ce94de49bca3f791abac2
Michael Watts <wattsjmichael@gmail.com>
2020-12-14 11:44:04 -0800

    added Middleware functionality
    Co-authored-by: Emme Buentiempo <emmettbuentiempo@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React  from "react";;
+import './App.css';
+import Headlines from "./Headlines";
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Headlines />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/middleware/middleware-logger.js
@@ -0,0 +1,8 @@
+coonst middlewareLogger = store => next => action => {
+  console.log("Original State", store.getState());
+  console.log("Current Action:", action);
+  next(action);
+  console.log('New Update State:', store.getState());
+};
+
+export default middlewareLogger;
